c0a218447efcf40ac42ce6c5cf28d6d515e55288
pradoav <a.rozputnii@gmail.com>
2018-10-04 22:02:35 +0300

    удаление коментов

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
 validates :title, :content, presence: :true
 mount_uploader :image, ImageUploader 
+ has_many :comments, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_181900) do
+ActiveRecord::Schema.define(version: 2018_10_04_174144) do
+
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
